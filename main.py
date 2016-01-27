4c7397224dafcdf4b2a491b3de33aae54273e017
Janosch Deurer <janosch.deurer@geonautik.de>
2016-01-27 14:05:39 +0100

    Better main

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ def run_deep_autoencoder():
     funcion docsting
     """
     np.random.seed(1337)  # for reproducibility
+    img_dim = 28*28
+    bottle_neck = 100
+    encoder_dim = 250
+    decoder_dim = 200
     batch_size = 128
-    nb_epoch = 100
+    nb_epoch = 2
     activation_fnc = 'relu'
 
     # the data, shuffled and split between train and test sets
@@ -29,28 +33,13 @@ def run_deep_autoencoder():
     x_train = x_train.astype("float32") / 255.0
     x_test = x_test.astype("float32") / 255.0
 
+    x_train = x_train[0:1000, :]
+    x_test = x_test[0:1000, :]
+
     print(x_train.shape[0], 'train samples')
     print(x_test.shape[0], 'test samples')
-
-bottle_neck = 500
-
-activation_fnc = 'relu'
-
-def compare_autoencoder_outputs(imgs, model, indices=[0], img_dim=(28,28)):
-    pred = model.predict(imgs)
-    for i in indices:
-        tup = (imgs[i].reshape(img_dim),pred[i].reshape(img_dim))
-        plt.matshow(tup[0])
-        plt.matshow(tup[1])
-    plt.show()
-
-
-def run_deep_autoencoder():
-    img_dim = 28*28
-    bottle_neck = 100
-    encoder_dim = 250
-    decoder_dim = 200
     # non-autoencoder
+
     model = Sequential()
     model.add(Dense(output_dim=encoder_dim, input_dim=img_dim,
                     activation=activation_fnc, init='uniform'))
@@ -67,7 +56,17 @@ def run_deep_autoencoder():
 
     # validation mit nearest neighbor
     # bottleneck small
-    compare_autoencoder_outputs(X_test, model, indices=[1,2,3,4])
+    compare_autoencoder_outputs(x_test, model, indices=[1, 2, 3, 4])
+
+
+def compare_autoencoder_outputs(imgs, model, indices=[0], img_dim=(28, 28)):
+    pred = model.predict(imgs)
+    for i in indices:
+        tup = (imgs[i].reshape(img_dim), pred[i].reshape(img_dim))
+        plt.matshow(tup[0])
+        plt.matshow(tup[1])
+    plt.show()
+
 
 if __name__ == "__main__":
     run_deep_autoencoder()
